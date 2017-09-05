1373ca5ee35736beb94dba0e4e0c9dddf7bef9bc
Dorian <398346921@qq.com>
2017-09-05 23:03:06 +0800

    initial commit

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,26 @@
+import originJSONP from 'jsonp'
+
+export default function jsonp(url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
+  return new Promise((resolve, reject) => {
+    originJSONP(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+function param(data) {
+  let url = ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += `&${k}=${encodeURIComponent(value)}`
+  }
+
+  // url字符串第一个字母为"&"，需要将之移除
+  return url ? url.substring(1) : ''
+}
